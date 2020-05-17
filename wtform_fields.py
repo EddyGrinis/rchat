98202188fde0ee8ef3d03ccc17fd3ecac7bfabef
EddyGrinis <grinise@gmail.com>
2020-05-17 20:47:18 +0300

    Add files via upload

--- wtform_fields.py
+++ wtform_fields.py
@@ -2,7 +2,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 
+#from form import LoginForm
 from models import User
+
+def invalid_credentials(form, field):
+    """ Username and password checker """
+
+    username_entered = form.username.data
+    password_entered = field.data
+
+    # Check credentials are valid
+    user_object = User.query.filter_by(username=username_entered).first()
+    if user_object is None:
+        raise ValidationError("Username or password is incorrect")
+    elif password_entered != user_object.password:
+        raise ValidationError("Username or password is incorrect")
 
 class RegistrationForm(FlaskForm):
     """ Registration form """
@@ -19,3 +33,12 @@ class RegistrationForm(FlaskForm):
         user_object = User.query.filter_by(username=username.data).first()
         if user_object:
             raise ValidationError("Username already exists. Select a different username.")
+
+class LoginForm(FlaskForm):
+    """ Login form """
+
+    username = StringField('username_label',
+        validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label',
+        validators=[InputRequired(message="Password required"), invalid_credentials])
+    submit_button = SubmitField('Login')
